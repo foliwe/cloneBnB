31907ccca2554b3627619bf94918254fbe2eb594
foliwe <foliwe@gmail.com>
2023-08-25 16:54:34 +0100

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ]
+  
+})
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container mx-auto">
+        <h1 class="text-4xl py-4">Index Page</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
